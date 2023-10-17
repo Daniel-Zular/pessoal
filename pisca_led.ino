2c47d39fa0859aa04411d34bcfc1abfb602301f6
Daniel-Zular <daniel.zular@sou.inteli.edu.br>
2023-10-17 11:12:07 -0300

    pisca

---
+++ pisca_led.ino
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(8, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(8, HIGH); //ligado
+  delay(500);                     
+  digitalWrite(8, LOW);   //desligado
+  delay(200);                      
+}
