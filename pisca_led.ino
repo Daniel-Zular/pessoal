90ca0a15cae82df097b20fcd7c96c833a46f0334
Daniel-Zular <123946756+Daniel-Zular@users.noreply.github.com>
2023-10-17 11:17:54 -0300

    Update pisca_led.ino

--- pisca_led.ino
+++ pisca_led.ino
@@ -9,3 +9,5 @@ void loop() {
   digitalWrite(8, LOW);   //desligado
   delay(200);                      
 }
+
+//link do tink https://www.tinkercad.com/things/aFKmd3l6POP-dazzling-trug-stantia/editel?sharecode=_LaxcYFyMC7FaKJxPblzQzNBLrFoLQjDU1cOv4ONPFg
